8fb6613ff5350b04de2a9a89584df9a67ddec238
abdurrahim bulut <abdurrahim.3078@icloud.com>
2021-07-16 19:39:21 +0300

    cache

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require './vendor/autoload.php';
 
+/*
 $client = new Predis\Client([
     'scheme' => 'tcp',
     'host'   => '127.0.0.1',
@@ -9,6 +10,48 @@ $client = new Predis\Client([
 ]);
 $client->set('foo', 'bar');
 $value = $client->get('foo');
-
 print_r($value);
+*/
+
+/*
+$client->hset("categories", "1", "deneme123");
+$client->hset("categories", "2", "deneme321");
+
+$value = $client->hgetall("categories");
+print_r($value);
+
+exit;
+*/
+try {
+    $db = new PDO("mysql:host=localhost;dbname=redis", "root", "");
+} catch ( PDOException $e ){
+    print $e->getMessage();
+}
+
+$client = new Predis\Client([
+    'scheme' => 'tcp',
+    'host'   => '127.0.0.1',
+    'port'   => 6379,
+]);
+
+
+if (!$client->exists('categories')) {
+    $query = $db->prepare("SELECT * FROM  categories");
+    $query->execute();
+    $categories = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($categories as $value) {
+        $client->hset("categories", $value['id'], $value['name']);
+    }
+}else {
+    $cache = $client->hgetall("categories");
+    $i=0;
+    foreach ($cache as $key => $value) {
+        $categories[$i]['id']=$key;
+        $categories[$i++]['name']=$value;
+    }
+}
+
+echo '<pre>';
+print_r($categories);
 
